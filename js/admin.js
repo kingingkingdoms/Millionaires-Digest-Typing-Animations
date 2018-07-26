e4f74fc14e9329d36aab1db0e311516e558ca296
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-07-26 15:30:49 -0400

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -14,28 +14,28 @@ jQuery(document).on('click', '.remove_typed_string', function () {
     //color field
     jQuery('.typed-color-field').wpColorPicker();
 
-    //typed admin perview
-    if (jQuery('.typed_perview_div').length > 0) {
-        var strings = jQuery('.typed_perview_div').attr('data-strings').split(';#$#;');
-        var speed = jQuery('.typed_perview_div').attr('data-speed');
-        var delay = jQuery('.typed_perview_div').attr('data-delay');
-        var back_delay = jQuery('.typed_perview_div').attr('data-back-delay');
-        var back_speed = jQuery('.typed_perview_div').attr('data-back-speed');
-        var loop = jQuery('.typed_perview_div').attr('data-loop');
+    //typed admin preview
+    if (jQuery('.typed_preview_div').length > 0) {
+        var strings = jQuery('.typed_preview_div').attr('data-strings').split(';#$#;');
+        var speed = jQuery('.typed_preview_div').attr('data-speed');
+        var delay = jQuery('.typed_preview_div').attr('data-delay');
+        var back_delay = jQuery('.typed_preview_div').attr('data-back-delay');
+        var back_speed = jQuery('.typed_preview_div').attr('data-back-speed');
+        var loop = jQuery('.typed_preview_div').attr('data-loop');
         if (loop == 'yes')
             loop = true
         else
             loop = false;
 
-        var cursor = jQuery('.typed_perview_div').attr('data-cursor');
+        var cursor = jQuery('.typed_preview_div').attr('data-cursor');
         if (cursor == 'yes')
             cursor = true
         else
             cursor = false;
 
-        var cursor_char = jQuery('.typed_perview_div').attr('data-cursor-char');
+        var cursor_char = jQuery('.typed_preview_div').attr('data-cursor-char');
 
-        jQuery(".typed_perview_div").typed({
+        jQuery(".typed_preview_div").typed({
             strings: strings,
             typeSpeed: parseInt(speed),
             // time before typing starts
